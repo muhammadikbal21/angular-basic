0b2b4701b8f2c0f81b1a5d283bf3613685668487
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-08-29 23:05:44 +0700

    penggunaan module untuk mengumpulkan beberapa component dalam 1 folder induk(parent)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './pages/toolbar/toolbar.component';
-import { ResourcesComponent } from './pages/resources/resources.component';
-import { NextStepsComponent } from './pages/next-steps/next-steps.component';
-import { FooterComponent } from './pages/footer/footer.component';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   // semua component akan dideklarasikan dibawah ini, agar dapat digunakan tag selectornya di html
   declarations: [
-    AppComponent,
-    ToolbarComponent,
-    ResourcesComponent,
-    NextStepsComponent,
-    FooterComponent
+    AppComponent
   ],
   // untuk mengimport package yang dibutuhkan pada component ini
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   // untuk service yang akan merequest dari backend
   providers: [],
