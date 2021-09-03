81894449653fb20b4f11d7b2ba844422b703373b
muhammadikbal21 <muhammadikbal21@gmail.com>
2021-09-03 10:59:19 +0700

    konsep dan alur penggunaan pipe

--- src/app/shared/pipes/join.pipe.ts
+++ src/app/shared/pipes/join.pipe.ts
@@ -7,7 +7,7 @@ export class JoinPipe implements PipeTransform {
 
   // parameter pertama adalah value yang menggunakan pipenya
   transform(value: string[] | null, delimiter: string = ', '): string {
-    return Array.isArray(value) ? value.join(delimiter) : '';
+    return Array.isArray(value) ? value.join(delimiter) : ''; // disini akan dicek apakah value adalah array, kalau true akan di join
     // value.join berfungsi untuk menggabungkan element-element array
     // join memiliki parameter yang berfungsi sebagai pemisah element-element yang akan dijoin atau digabungkan
   }
